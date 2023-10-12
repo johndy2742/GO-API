24bed7ad4fa9c95d3931b83c6702c61c572a937d
johndy2742 <johndypanca12345@gmail.com>
2023-10-12 12:10:21 +0700

    remove temporary memory and add local postgresql database

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,73 @@
-// main.go
-
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+
 	"api/delivery"
 	"api/repository"
 	"api/usecase"
 
-	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
+type Config struct {
+	PGUser     string `json:"PG_USER"`
+	PGPassword string `json:"PG_PASSWORD"`
+	PGDBName   string `json:"PG_DBNAME"`
+}
+
+func loadConfig() (*Config, error) {
+	configFile, err := os.Open("../config/config.json")
+	if err != nil {
+		return nil, err
+	}
+	defer configFile.Close()
+
+	var config Config
+	decoder := json.NewDecoder(configFile)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
 func main() {
-	// Initialize the repository
-	bookRepository := repository.NewInMemoryBookRepository()
+	// Load configuration
+	config, err := loadConfig()
+	if err != nil {
+		fmt.Println("Failed to load configuration:", err)
+		return
+	}
+
+	// Construct the PostgreSQL connection string
+	connString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		config.PGUser, config.PGPassword, config.PGDBName)
+
+	// Connect to PostgreSQL
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		fmt.Println("Failed to connect to PostgreSQL:", err)
+		return
+	}
+	defer db.Close()
+
+	// Try to ping the database to check the connection
+	if err = db.Ping(); err != nil {
+		fmt.Println("Failed to ping the database:", err)
+		return
+	}
+
+	fmt.Println("Successfully connected to the database")
+
+	// Initialize the PostgreSQL repository
+	bookRepository := repository.NewPostgreSQLBookRepository(db)
 
 	// Initialize the usecase
 	bookUseCase := usecase.NewBookUseCase(bookRepository)
